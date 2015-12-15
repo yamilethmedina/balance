91be9f082022ced3e3604deb8853b46abe8f6fff
Yami Medina <yamilethmedina1@gmail.com>
2015-12-15 00:49:51 +0000

    added FL handler

--- lib/state_handler/fl.rb
+++ lib/state_handler/fl.rb
@@ -1,17 +1,18 @@
-# Step 0. Change "::Example" below to a state abbreviation
+# Step 1. Change "::Example" below to a state abbreviation
 # For example, "::PA" for Pennsylvania
 class StateHandler::FL < StateHandler::Base
 
-  # Step 1. EXAMPLE — Edit for your state!
-  PHONE_NUMBER = '+8637346192'
-
   # Step 2. EXAMPLE — Edit for your state!
+  PHONE_NUMBER = '+18883563281'
+
+  # Step 3. EXAMPLE — Edit for your state!
   ALLOWED_NUMBER_OF_EBT_CARD_DIGITS = [16]
 
   def button_sequence(ebt_number)
-    # Step 3. EXAMPLE — Edit for your state!
-    "ww1ww#{ebt_number}"
+    # Step 4. EXAMPLE — Edit for your state!
+    "wwww1wwww#{ebt_number}"
   end
+
 
   def transcribe_balance_response(transcription_text, language = :english)
     mg = MessageGenerator.new(language)
@@ -23,17 +24,18 @@ class StateHandler::FL < StateHandler::Base
     end
 
     # Deal with an invalid card number
-    ### Step 4. EXAMPLE — Edit for your state! ###
-    phrase_indicating_invalid_card_number = "we did not get your card number"
+
+    ### Step 5. EXAMPLE — Edit for your state! ###
+    phrase_indicating_invalid_card_number = "CHANGE ME"
+
 
     if transcription_text.include?(phrase_indicating_invalid_card_number)
       return mg.card_number_not_found_message
     end
 
-    # Deal with a successful balance transcription
-    ### Step 5. EXAMPLE — Edit for your state! ###
+      ### Step 6. EXAMPLE — Edit for your state! ###
     regex_matches = transcription_text.scan(/(\$\S+)/)
-    if regex_matches.count > 1
+    if regex_matches.count > 0
       ebt_amount = regex_matches[0][0]
       return "Hi! Your food stamp balance is #{ebt_amount}."
     end
